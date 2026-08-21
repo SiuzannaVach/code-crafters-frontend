e6b045613cbe3078b6d4e03d36a92987d9c5cafc
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-08-21 17:46:55 +0200

    style: complete register modal pixel-perfect design for mobile and desktop

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import styles from './styles/global.module.scss';
+import { RegisterModal } from './components/RegisterModal/RegisterModal';
 
 function App() {
   return (
     <div className={styles.pageLayout}>
       {/* Верхняя панель навигации */}
       <Header isAuthenticated={true} />
-      
+      <RegisterModal />
+
       {/* Сюда мы будем вставлять настоящие страницы или карточки событий */}
       <main style={{ padding: '2rem', flex: '1' }}>
         {/* Текст удален. Теперь здесь пустое рабочее пространство */}
