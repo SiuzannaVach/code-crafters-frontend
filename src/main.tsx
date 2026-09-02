60b068ca11808a1fc6984815c00de5f466156e90
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-09-02 12:56:25 +0200

    chore: add automatic localStorage cleanup script in development mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.module.scss';
-
 import App from './App.tsx'
+
+if (import.meta.env.DEV) {
+  localStorage.clear();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
