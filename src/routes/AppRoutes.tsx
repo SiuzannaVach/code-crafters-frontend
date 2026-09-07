9add167fa5e82e89f7190dfa36f6004be301dc67
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-09-07 13:55:18 +0200

    feat(routes): update application route configuration and layout guards

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { LoginCard } from '../pages/Login/LoginCard';
-import { RegisterModal } from '../components/RegisterModal/RegisterModal';
-import CreateEvent from '../pages/CreateEvent/CreateEvent';
-import Home from '../pages/Home/Home';
-import AuthenticatedLayout from '../components/AuthenticatedLayout/AuthenticatedLayout';
-import EventDetail from '../pages/EventDetail/EventDetail'; 
-import { Dashboard } from '../pages/Dashboard/Dashboard';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginCard } from "../pages/Login/LoginCard";
+import { RegisterModal } from "../components/RegisterModal/RegisterModal";
+import CreateEvent from "../pages/CreateEvent/CreateEvent";
+import Home from "../pages/Home/Home";
+import AuthenticatedLayout from "../components/AuthenticatedLayout/AuthenticatedLayout";
+import EventDetail from "../pages/EventDetail/EventDetail";
+import { Dashboard } from "../pages/Dashboard/Dashboard";
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/login" element={<LoginCard />} />
       <Route path="/register" element={<RegisterModal />} />
 
-      
       <Route element={<AuthenticatedLayout />}>
         <Route path="/home" element={<Home />} />
         <Route path="/events/:id" element={<EventDetail />} />
         <Route path="/create-event" element={<CreateEvent />} />
-        
-       
+
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
 
-     
       <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   );
