f1b0f3dd0519a1c278025b6f78d8c29688ab0e48
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-09-07 19:44:53 +0200

    chore(main): minor updates to application entry point tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.module.scss';
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.module.scss";
+import App from "./App.tsx";
 
-if (import.meta.env.DEV) {
-  localStorage.clear();
-}
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
