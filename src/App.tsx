47299379547ea824eec9c93ea1176fe572123a4e
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-09-09 16:17:38 +0200

    fix: configure base URL and HashRouter for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import styles from './styles/global.module.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className={styles.pageLayout}>
         <main className={styles.mainContent}>
           <AppRoutes />
         </main>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
